cf0230c5e14ebd04b7154fb86dc12aed7c7dca41
quellen-sol <52678959+quellen-sol@users.noreply.github.com>
2024-08-01 13:56:55 -0600

    Support wildcard in geyser config

--- crates/plugin/src/selectors.rs
+++ crates/plugin/src/selectors.rs
@@ -13,10 +13,12 @@ use crate::{
 
 #[derive(Debug)]
 pub struct TransactionSelector {
+    /// K = Program, V = routing_key
     programs: HashMap<Pubkey, Arc<String>>,
+    /// K = Program, V = routing_key
     pubkeys: HashMap<Pubkey, Arc<String>>,
-    /// K = routing prefix, V = allows all?
-    allows_all: HashMap<String, bool>,
+    /// K = routing prefix, V = allows all programs?
+    allows_all_programs: Vec<Arc<String>>,
     multi_routing_key: Arc<String>,
 }
 
@@ -24,27 +26,27 @@ impl TransactionSelector {
     pub fn from_config(config: Transactions) -> Result<Self> {
         let Transactions { programs, pubkeys } = config;
 
+        let allows_all_programs = programs
+            .iter()
+            .filter_map(|(pk, rk)| if pk == "*" { Some(rk) } else { None })
+            .map(|s| Self::make_routing_key(s))
+            .collect();
+
         let programs = programs
             .into_iter()
-            .filter_map(|s| {
-                s.0.parse()
-                    .map(|a| (a, Arc::new(format!("{}.transaction", s.1))))
-                    .ok()
-            })
+            .filter_map(|s| s.0.parse().map(|a| (a, Self::make_routing_key(&s.1))).ok())
             .collect::<_>();
 
         let pubkeys = pubkeys
             .into_iter()
-            .map(|s| {
-                s.0.parse()
-                    .map(|a| (a, Arc::new(format!("{}.transaction", s.1))))
-            })
+            .map(|s| s.0.parse().map(|a| (a, Self::make_routing_key(&s.1))))
             .collect::<Result<_, _>>()
             .context("Failed to parse tx pubkeys")?;
 
         Ok(Self {
             programs,
             pubkeys,
+            allows_all_programs,
             multi_routing_key: Arc::new("multi.transaction".to_string()),
         })
     }
@@ -52,6 +54,11 @@ impl TransactionSelector {
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.programs.is_empty() && self.pubkeys.is_empty()
+    }
+
+    #[inline]
+    fn make_routing_key(s: &str) -> Arc<String> {
+        Arc::new(format!("{}.transaction", s))
     }
 
     #[inline]
@@ -86,6 +93,7 @@ impl TransactionSelector {
             .unique()
             .filter_map(|a| keys.get(a))
             .filter_map(|a| self.programs.get(a))
+            .chain(self.allows_all_programs.iter())
             .unique()
             .take(2); //if > 1 then we use multi anyhow
 
