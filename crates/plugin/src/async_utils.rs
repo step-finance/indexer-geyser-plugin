58ede03eb1580d7a2803bad4a7a796cb27a7941c
quellen-sol <52678959+quellen-sol@users.noreply.github.com>
2025-09-03 13:16:45 -0600

    more robust future streaming

--- crates/plugin/src/async_utils.rs
+++ crates/plugin/src/async_utils.rs
@@ -1,29 +1,5 @@
 use std::future::Future;
 
-pub struct IntervalTimer {
-    pub interval: std::time::Duration,
-    last_timestamp: std::time::Instant,
-}
-
-impl IntervalTimer {
-    pub fn new(interval: std::time::Duration) -> Self {
-        Self {
-            last_timestamp: std::time::Instant::now(),
-            interval,
-        }
-    }
-
-    pub fn interval_hit(&mut self) -> bool {
-        let elapsed = self.last_timestamp.elapsed();
-        if elapsed >= self.interval {
-            self.last_timestamp = std::time::Instant::now();
-            true
-        } else {
-            false
-        }
-    }
-}
-
 pub fn run_future_on_new_thread<T: Send + 'static>(
     fut: impl Future<Output = T> + Send + 'static,
     runtime: tokio::runtime::Runtime,
--- crates/plugin/src/message_processor.rs
+++ crates/plugin/src/message_processor.rs
@@ -1,25 +1,46 @@
 use std::{sync::Arc, time::Duration};
 
+use crossbeam::channel::RecvTimeoutError;
 use futures::{stream::FuturesUnordered, StreamExt};
 use indexer_rabbitmq::geyser::Message;
 
-use crate::{async_utils::IntervalTimer, sender::Sender};
+use crate::sender::Sender;
 
 pub async fn run_message_publisher(
     receiver: crossbeam::channel::Receiver<(Message, String)>,
     sender: Arc<Sender>,
     max_running_futures: usize,
 ) {
-    // timer to ensure we don't get stuck if the validator stops, and we have `< max_running_futures` in the queue
-    let mut timer = IntervalTimer::new(Duration::from_millis(10));
     let mut futs = FuturesUnordered::new();
 
-    // Loop that will automatically end when `receiver` closes due to shutdown signal
-    while let Ok((msg, route)) = receiver.recv() {
-        let fut = sender.send(msg, route);
-        futs.push(fut);
-        if futs.len() >= max_running_futures || timer.interval_hit() {
-            futs.next().await;
+    loop {
+        match receiver.recv_timeout(Duration::from_millis(10)) {
+            Ok((msg, route)) => {
+                let fut = sender.send(msg, route);
+                futs.push(fut);
+                if futs.len() >= max_running_futures {
+                    futs.next().await;
+                }
+            },
+            Err(e) => match e {
+                RecvTimeoutError::Timeout => {
+                    if sender.is_stopped() {
+                        // Plugin is stopped, drain remaining futures
+                        while let Some(()) = futs.next().await {}
+                        break;
+                    }
+
+                    // Validator stopped??
+                    // Either way, just wait out the next future, so we're not limited by the max above
+                    // and we also get as many messages sent out as possible
+                    futs.next().await;
+                },
+                RecvTimeoutError::Disconnected => {
+                    // Channel is closed, Drain remaining futures, and break out of loop
+                    while let Some(()) = futs.next().await {}
+                    break;
+                },
+            },
         }
     }
 
